4b6859c20086d640f0767e290df5bb6c88bace5c
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-10 17:38:31 -0500

    [FIX] Fixing shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """app module"""
 from flask import Flask, jsonify
 from models import storage
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """init file"""
 from flask import Blueprint
 
