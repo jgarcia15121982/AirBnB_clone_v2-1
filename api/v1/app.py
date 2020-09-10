b0640d299bd51997e1e11e3d1742a72d3c6feebe
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-10 13:19:44 -0500

    [ADD] adding api directory

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""app module"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def clse(self):
+    """docs"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_fount(e):
+    """Page not found"""
+    return jsonify({"error": "Not found"})
+
+
+if __name__ == '__main__':
+    """docs"""
+    hst = getenv('HBNB_API_HOST')
+    prt = getenv('HBNB_API_PORT')
+    if hst is None:
+        hst = '0.0.0.0'
+    if prt is None:
+        prt = 5000
+    app.run(host=hst, port=prt, threaded=True, debug=True)
