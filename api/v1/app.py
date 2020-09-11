b37aafca9d80129bcf70fee788c7151e0a41e889
AdrianCHdz <1214@holbertonschool.com>
2020-09-11 04:42:57 +0000

    [Feature]: Adding CORS http access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
